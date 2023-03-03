f9d069d09acbc7a48e3f1c502e75258b027dfbaa
3DFIN <3dfinsoftware@gmail.com>
2023-03-03 12:03:58 +0000

    Update ground.py
    Updated docstring to generate automatic documentation with Sphinx (numpy style).

--- src/dendromatics/ground.py
+++ src/dendromatics/ground.py
@@ -1,6 +1,7 @@
 #### IMPORTS ####
 import CSF
 import numpy as np
+from scipy.spatial import cKDTree
 from sklearn.cluster import DBSCAN
 
 from .voxel.voxel import *
@@ -11,26 +12,21 @@ from .voxel.voxel import *
 
 
 def clean_ground(cloud):
-    """
-    ---------------------------------------------------------------------------
-    ------------------           General description           ----------------
-    ---------------------------------------------------------------------------
+    """ This function takes a point cloud and denoises it via DBSCAN 
+    clustering. It first voxelates the point cloud into 0.15 m voxels, then 
+    clusters the voxel cloud and excludes clusters of size less than 2.
 
-    This function takes a point cloud and denoises it via DBSCAN clustering. It
-    first voxelates the point cloud into 0.15 m voxels, then clusters the voxel
-    cloud and excludes clusters of size less than 2.
-
-    ---------------------------------------------------------------------------
-    ------------------                 Inputs                  ----------------
-    ---------------------------------------------------------------------------
-
-    cloud: numpy array. Matrix containing (x, y, z) coordinates of the points.
-
-    -----------------------------------------------------------------------------
-    -----------------                 Outputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    clust_cloud: numpy array. Matrix containing (x, y, z) coordinates of the denoised points.
+    Parameters
+    ----------
+    cloud : numpy.ndarray
+        The point cloud to be denoised. Matrix containing (x, y, z) coordinates
+        of the points.
+    
+    Returns
+    -------
+    clust_cloud : numpy.ndarray
+        The denoised point cloud. Matrix containing (x, y, z) coordinates of 
+        the denoised points.
     """
 
     vox_cloud, vox_to_cloud_ind, cloud_to_vox_ind = voxelate(
@@ -58,12 +54,13 @@ def clean_ground(cloud):
 
     # Removing the points that are not in valid clusters.
     clust_cloud = cloud_labs[np.isin(cloud_labs[:, -1], large_clusters), :3]
+    
     return clust_cloud
 
 
-# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # classify_ground
-# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def generate_dtm(
@@ -73,28 +70,32 @@ def generate_dtm(
     classify_threshold=0.1,
     exportCloth=True,
 ):
-    """
-    -----------------------------------------------------------------------------
-    ------------------           General description           ------------------
-    -----------------------------------------------------------------------------
-
-    This function takes a point cloud and generates a Digital Terrain Model (DTM) based on its ground.
-    It's based on 'Cloth Simulation Filter' by: W. Zhang et al., 2016 (http://www.mdpi.com/2072-4292/8/6/501/htm),
-    which is implemented on CSF package. This function just implements it in a convenient way for this use-case.
-    -----------------------------------------------------------------------------
-    ------------------                 Inputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    cloud: numpy array. Matrix containing (x, y, z) coordinates of the points.
-    bSloopSmooth: Boolean. default value: True. The resulting DTM will be smoothed. Refer to CSF documentation.
-    cloth_resolution: float. default value: 0.5. The resolution of the cloth grid. Refer to CSF documentation.
-    classify_threshold: float. default value: 0.1. The threshold used to classify the point cloud into ground and non-ground parts. Refer to CSF documentation.
-    exportCloth: Boolean. default value = True. The DTM will be exported. Refer to CSF documentation.
-    -----------------------------------------------------------------------------
-    -----------------                 Outputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    cloth_nodes: numpy array. Matrix containing (x, y, z) coordinates of the DTM points.
+    """ This function takes a point cloud and generates a Digital Terrain Model
+    (DTM) based on its ground. It's based on 'Cloth Simulation Filter' by 
+    W. Zhang et al., 2016 (http://www.mdpi.com/2072-4292/8/6/501/htm),
+    which is implemented in CSF package. This function just implements it in a 
+    convenient way for this use-case.
+    
+    Parameters
+    ----------
+    cloud : numpy.ndarray
+        The point cloud. Matrix containing (x, y, z) coordinates of the points.
+    bSloopSmooth : Boolean
+        The resulting DTM will be smoothed. Refer to CSF documentation. 
+        Defaults to True.    
+    cloth_resolution : float
+        The resolution of the cloth grid. Refer to CSF documentation. Defaults
+        to 0.5.
+    classify_threshold : float
+        The height threshold used to classify the point cloud into ground and 
+        non-ground parts. Refer to CSF documentation. Defaults to 0.1.
+    exportCloth : Boolean
+        The DTM will be exported. Refer to CSF documentation. Defaults to True.
+    
+    Returns
+    -------
+    cloth_nodes : numpy.ndarray
+        Matrix containing (x, y, z) coordinates of the DTM points.
     """
 
     ### Cloth simulation filter ###
@@ -127,76 +128,62 @@ def generate_dtm(
     return cloth_nodes
 
 
-# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # clean_cloth
-# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def clean_cloth(dtm_points):
-    """
-    -----------------------------------------------------------------------------
-    ------------------           General description           ------------------
-    -----------------------------------------------------------------------------
-
-    This function takes a Digital Terrain Model (DTM) and denoises it. This 
-    denoising is done via a 2 MADs from the median height value of a 
+    """ This function takes a Digital Terrain Model (DTM) and denoises it. This 
+    denoising is done via a 2 MADs criterion from the median height value of a 
     neighbourhood of size 15.
-    -----------------------------------------------------------------------------
-    ------------------                 Inputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    dtm_points: numpy array. Matrix containing (x, y, z) coordinates of the DTM
-    points.
-    -----------------------------------------------------------------------------
-    -----------------                 Outputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    clean_points: numpy array. Matrix containing (x, y, z) coordinates of the 
-    denoised DTM points.
+    
+    Parameters
+    ----------
+    dtm_points : numpy.ndarray
+        Matrix containing (x, y, z) coordinates of the DTM points.
+    
+    Returns
+    -------
+    clean_points : numpy.ndarray
+        Matrix containing (x, y, z) coordinates of the denoised DTM points.
     """
-
-    from scipy.spatial import cKDTree
 
     tree = cKDTree(dtm_points[:, :2])
     d, indexes = tree.query(dtm_points[:, :2], 15)
     abs_devs = np.abs(dtm_points[:, 2] - np.median(dtm_points[:, 2][indexes], axis=1))
     mads = np.median(abs_devs)
     clean_points = dtm_points[abs_devs < 2 * mads]
+    
     return clean_points
 
 
-# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # normalize_heights
-# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 def normalize_heights(cloud, dtm_points):
+    """ This function takes a point cloud and a Digital Terrain Model (DTM) and
+    normalizes the heights of the first based on the second.
+    
+    Parameters
+    ----------
+    cloud : numpy.ndarray
+        Matrix containing (x, y, z) coordinates of the point cloud.
+    dtm_points : numpy array
+        Matrix containing (x, y, z) coordinates of the DTM points.
+
+    Returns
+    -------
+    zs_diff_triples : numpy.ndarray
+        Vector containing the normalized height values for the cloud points.
     """
-    -----------------------------------------------------------------------------
-    ------------------           General description           ------------------
-    -----------------------------------------------------------------------------
-
-    This function takes a point cloud and a Digital Terrain Model (DTM) and normalizes the heights
-    of the first based on the second.
-    -----------------------------------------------------------------------------
-    ------------------                 Inputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    cloud: numpy array. Matrix containing (x, y, z) coordinates of the point cloud.
-    dtm_points: numpy array. Matrix containing (x, y, z) coordinates of the DTM points.
-
-    -----------------------------------------------------------------------------
-    -----------------                 Outputs                  ------------------
-    -----------------------------------------------------------------------------
-
-    zs_diff_triples: numpy array. Vector containing the normalized height values for the points in 'cloud'.
-    """
-    from scipy.spatial import cKDTree
-
+    
     tree = cKDTree(dtm_points[:, :2])
     d, idx_pt_mesh = tree.query(cloud[:, :2], 3)
     # Z point cloud - Z dtm (Weighted average, based on distance)
     zs_diff_triples = cloud[:, 2] - np.average(
         dtm_points[:, 2][idx_pt_mesh], weights=d, axis=1
     )
-    return zs_diff_triples  # vector containing the normalized heights for the points in point_cloud_xyz
+    return zs_diff_triples
