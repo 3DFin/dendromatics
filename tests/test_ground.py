603257be703bf35929960c071817ddb7e98735da
3DFIN <3dfinsoftware@gmail.com>
2024-01-16 15:34:44 +0100

    test cases for clean_cloth
    New test cases have been added to `test_ground.py` module to cover `clean_cloth()` function.

--- tests/test_ground.py
+++ tests/test_ground.py
@@ -27,6 +27,93 @@ def test_clean_ground():
     np.testing.assert_array_almost_equal(output, expect)
 
 
+def test_clean_cloth_with_noise():
+    # Regular grid of points expanding from (0, 0) to (1, 1)
+    x = np.linspace(0.0, 1.0, 11)
+    y = np.linspace(0.0, 1.0, 11)
+
+    xy = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+    # adding a constant Z field (z = 0) to all points in the grid
+    z = np.expand_dims(np.zeros(121), axis=1)
+
+    cloud = np.append(xy, z, axis=1)
+
+    # Adding noise/outlier: point outside the grid, at z = 2
+    z_outlier = np.array([[0.5, 0.5, 2]])
+
+    noisy_cloud = np.append(cloud, z_outlier, axis=0)
+
+    # Expect is the regular grid
+    expect = np.append(xy, z, axis=1)
+
+    # Output should be the noisy cloud minus the outlier point --> the regular grid
+    output = dm.clean_cloud(noisy_cloud)
+
+    np.testing.assert_array_almost_equal(output, expect)
+
+
+def test_clean_cloth_already_is_clean():
+    # Regular grid of points expanding from (0, 0) to (1, 1)
+    x = np.linspace(0.0, 1.0, 11)
+    y = np.linspace(0.0, 1.0, 11)
+
+    xy = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+    # adding a constant Z field (z = 0) to all points in the grid
+    z = np.expand_dims(np.zeros(121), axis=1)
+
+    cloud = np.append(xy, z, axis=1)
+
+    # Expect is the regular grid
+    expect = np.append(xy, z, axis=1)
+
+    # Output should be the regular grid --> no points removed
+    output = dm.clean_cloud(cloud)
+
+    np.testing.assert_array_almost_equal(output, expect)
+
+
+def test_clean_cloth_small_cloth():
+    # Regular grid of points expanding from (0, 0) to (1, 1) (ONLY 9 points)
+    x = np.linspace(0.0, 1.0, 3)
+    y = np.linspace(0.0, 1.0, 3)
+
+    xy = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+    # adding a constant Z field (z = 0) to all points in the grid
+    z = np.expand_dims(np.zeros(9), axis=1)
+
+    cloud = np.append(xy, z, axis=1)
+
+    # function should not work as it performs a 15-nearest-neighbours search.
+    np.testing.assert_raises(ValueError, clean_cloth, cloud)
+
+
+def test_clean_cloth_barely_big_cloth():
+    # Regular grid of points expanding from (0, 0) to (1, 1) (16 points)
+    x = np.linspace(0.0, 1.0, 4)
+    y = np.linspace(0.0, 1.0, 4)
+
+    xy = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+    # adding a constant Z field (z = 0) to all points in the grid
+    z = np.expand_dims(np.zeros(9), axis=1)
+
+    cloud = np.append(xy, z, axis=1)
+
+    # two points are removed, so there are 14 points
+    cloud = cloud[0:(cloud.shape[0] - 2)]
+
+    # Adding noise/outlier: point outside the grid, at z = 2
+    z_outlier = np.array([[0.5, 0.5, 2]])
+
+    # cloud now has just 15 points
+    noisy_cloud = np.append(cloud, z_outlier, axis=0)
+
+    np.testing.assert_warns(Warning, clean_cloth, noisy_cloud)
+
+
 def test_normalize_heights():
     # DTM:
     # Regular grid of points expanding from (0, 0) to (1, 1)
